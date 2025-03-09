d6e4a8f66bf0e836a80f733e39f0db06afe1b448
Everest Paudel <paudeleverest09@gmail.com>
2025-03-09 21:47:47 +0545

    Learning :Wrapper Components

---
+++ src/components/Card.tsx
@@ -0,0 +1,42 @@
+import { Children } from "react";
+import { View ,Text,StyleSheet} from "react-native";
+import { PropsWithChildren } from "react";
+
+type Card = PropsWithChildren<{
+    title:string
+    children :React.ReactNode
+}>
+
+export default function Card({title,children}:PropsWithChildren<Card>){
+    return (
+       <View style ={styles.questionCard}>
+                  
+                   <Text style={styles.title}>{title}</Text>  
+                   {children}
+                   </View>
+    )
+}  
+ const styles = StyleSheet.create({
+    questionCard :{
+        backgroundColor :'white',
+        padding :20,
+        paddingVertical :40,
+        borderRadius :20,
+        gap :20,
+
+        // shadow
+        shadowColor: "#000",
+shadowOffset: {
+	width: 0,
+	height: 2,
+},
+shadowOpacity: 0.25,
+shadowRadius: 3.84,
+
+elevation: 5,
+    },
+    title:{
+fontSize :24,
+fontWeight :'400',
+    },
+ })
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,6 +1,7 @@
 import { View ,Text,StyleSheet} from "react-native";
 import AnswerOption from "./AnswerOption";
 import { Question } from "../type";
+import Card from "./Card";
 
 
 
@@ -9,49 +10,21 @@ type QuestionCard ={
 }
 
 
-export default function QuestionCard({question}:QuestionCard){
+export default function QuestionCard({question,children}:QuestionCard){
     const  selectedOption = question.options[0]
     const onOptionSelected =(option:string)=>{
         console.log("selected",option)
     }
     return (
-        <View style ={styles.questionCard}>
-            <View>
-                
-            </View>
-            <Text style={styles.question}>{question.title}</Text>
-            
-               <View style={{gap:10}}> 
+       
+            <Card title={question.title}>
+            <View style={{gap:10}}> 
                 {question.options.map((option,index)=>  <AnswerOption option={question.options[index]} isSelected={question.options[index] === selectedOption} onPress={()=>onOptionSelected(option)}/> )}
              
                </View>
               
-           
-        </View>
+            </Card>
+          
     )
 }
 
-const styles =StyleSheet.create({
-    questionCard :{
-        backgroundColor :'white',
-        padding :20,
-        paddingVertical :40,
-        borderRadius :20,
-        gap :20,
-
-        // shadow
-        shadowColor: "#000",
-shadowOffset: {
-	width: 0,
-	height: 2,
-},
-shadowOpacity: 0.25,
-shadowRadius: 3.84,
-
-elevation: 5,
-    },
-    question:{
-fontSize :24,
-fontWeight :'400',
-    },
-})
